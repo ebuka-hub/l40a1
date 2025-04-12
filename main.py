e6040da42aeeac266bbba81ef8fa80c6474c251f
ebuka-hub <albertebuka69@gmail.com>
2025-04-12 06:53:35 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+class Employee:
+
+    #Initailizing (Constructor)
+    def __init__(self):
+    print('Employee created. ')
+
+    #deleting (Desturtor)
+    def __init__(self):
+        print('Destructor called, Employee deleted')
